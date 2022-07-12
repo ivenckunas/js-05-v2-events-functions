8abc5df4bddc41a1af39e1526fc7c16935c0187a
ivenckunas <ignas.venckunas@gmail.com>
2022-07-12 15:22:15 +0300

    onclick, addEventListener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("main.js");
+// console.log("main.js");
+// nusitaikymai
+const btn1El = document.getElementById("btn-click");
+
+// onclick
+
+let count = 0;
+
+function showClick() {
+  count++;
+  console.log(`Clicked ${count} times`);
+}
+
+// addEventListener
+
+btn1El.addEventListener("click", showClick);
+// btn1El.addEventListener("mouseenter", showClick);
